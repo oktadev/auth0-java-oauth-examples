5f4973f5bd179a9bb4e5a79b86ff34a6b4365213
Matt Raible <matt@raibledesigns.com>
2023-05-02 09:31:49 -0600

    Upgrade to Spring Boot 3.0.6, Micronaut 3.9.1, and Quarkus 3.0.1

--- quarkus/src/main/java/com/example/ProfileResource.java
+++ quarkus/src/main/java/com/example/ProfileResource.java
@@ -5,13 +5,13 @@ import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
 import io.quarkus.security.Authenticated;
 
-import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.SecurityContext;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.SecurityContext;
 
 /**
  * Displays the current user's OIDC claims.
