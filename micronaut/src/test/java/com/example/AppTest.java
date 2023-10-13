d2cfb9f2ad486d70b27b56646edc3ecc13876c5e
Matt Raible <matt@raibledesigns.com>
2023-10-13 14:01:26 -0600

    Upgrade to Spring Boot 3.1.4, Micronaut 4.0.4, and Quarkus 3.4.3

--- micronaut/src/test/java/com/example/AppTest.java
+++ micronaut/src/test/java/com/example/AppTest.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import io.micronaut.context.annotation.Property;
+import io.micronaut.core.util.StringUtils;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
@@ -7,6 +9,7 @@ import org.junit.jupiter.api.Assertions;
 
 import jakarta.inject.Inject;
 
+@Property(name = "micronaut.security.enabled", value = StringUtils.FALSE)
 @MicronautTest
 class AppTest {
 
